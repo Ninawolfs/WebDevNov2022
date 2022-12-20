ede5f97aa2b12839430f4853a23197cae30098ff
Ninawolfs <nina.wolfs.nw@gmail.com>
2022-12-20 12:14:13 +0100

    update

---
+++ Basics/html/JavaScript1/index.js
@@ -0,0 +1,52 @@
+// console.log('Whats uuuuuuuuup');
+
+// // alert('Danger danger')
+
+// console.log("1"+1)
+
+// console.log("hel"+"lo"+" world");
+
+// console.log(12/4);
+
+// console.log(12%5);
+
+// console.log(typeof 'hello');
+
+
+
+// //variabletype variablename=value/data
+
+// var fullname="Joe Dalton"
+// // var 1234fullname="Michael Jordan" nooit met cijfersstarten
+// var _a="Hello"
+// // var FULLNAME="Joe Doe"  --> Hoofdlettergevoelig!
+// // var                 x               ="4"   -->spatie niet opgemerkt maar best niet doen voor leesbaarheid
+
+
+
+// variable types:
+/*
+Let
+const --> statisch, constant, unchangable
+var
+*/
+
+// console.log(y, "before");
+// var y=5;
+// console.log(y, "before update");
+// y=10; //value
+// console.log(y, "after update");
+//  var y="hello world" //re-declaration
+// console.log(y, "after updated value");
+
+
+
+var companyname="Monizze" //declaration of variable
+console.log(companyname);
+// update
+companyname="intec" //update variable value --> replace without overwriting (old value is remembered)
+console.log(companyname);
+
+//re-declaration --> overwriting the value (forget old value)
+var companyname="the oracle"
+console.log(companyname);
